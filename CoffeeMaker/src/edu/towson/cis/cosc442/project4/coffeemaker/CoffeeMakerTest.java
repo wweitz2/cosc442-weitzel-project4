8ae48c6b062335ecb892d01ecb596801a118d00d
Wes Weitzel <wweitzel@mindgrub.com>
2017-03-19 18:35:30 -0400

    Task 3: Updated Acceptance Test word document

--- CoffeeMaker/src/edu/towson/cis/cosc442/project4/coffeemaker/CoffeeMakerTest.java
+++ CoffeeMaker/src/edu/towson/cis/cosc442/project4/coffeemaker/CoffeeMakerTest.java
@@ -17,10 +17,11 @@ public class CoffeeMakerTest extends TestCase {
 		r1 = new Recipe();
 		r1.setName("Coffee");
 		r1.setPrice(50);
-		r1.setAmtCoffee(6);
+		r1.setAmtCoffee(3);
 		r1.setAmtMilk(1);
 		r1.setAmtSugar(1);
 		r1.setAmtChocolate(0);
+		
 	}
 
 	public void testAddRecipe1() {
